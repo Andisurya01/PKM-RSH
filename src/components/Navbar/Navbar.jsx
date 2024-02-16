e4c97abb813e32df770df0e8421c500fe5c5056d
Andisurya01 <andi.suryapriyadi@gmail.com>
2024-02-16 10:54:19 +0700

    edit nama aplikasi

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <nav className="flex justify-between py-4 px-5">
-            <h1 className="font-extrabold">Madani</h1>
+            <h1 className="font-extrabold">Bioexplore</h1>
             <div className="flex gap-5">
                 <p className="font-semibold">About</p>
                 <p className="font-semibold">Services</p>
